6117ae28a2838518eeb3efce7dc0b5e4c9920662
Ercan ALTUN <salsel0147@gmail.com>
2022-08-17 16:35:45 +0300

    con1

---
+++ src/git_Confilicts/A01_LocaldeConfilictOlusturma.java
@@ -0,0 +1,9 @@
+package git_Confilicts;
+
+public class A01_LocaldeConfilictOlusturma {
+    public static void main(String[] args) {
+
+        System.out.println("Lokalimizde 1. confilict denemesi");
+
+    }
+}
